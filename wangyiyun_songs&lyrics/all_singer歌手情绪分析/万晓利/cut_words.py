e8fa055b2ec96dc41dc0101534cf189c8693946c
Python_web crawler <31296722+cassieeric@users.noreply.github.com>
2019-03-22 12:35:45 +0800

    Add files via upload

---
+++ wangyiyun_songs&lyrics/all_singer歌手情绪分析/万晓利/cut_words.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import jieba
+
+
+# jieba.load_userdict('userdict.txt')
+# 创建停用词list
+def stopwordslist(filepath):
+    stopwords = [line.strip() for line in open(filepath, 'r', encoding='gbk').readlines()]
+    return stopwords
+
+
+# 对句子进行分词
+def seg_sentence(sentence):
+    sentence_seged = jieba.cut(sentence.strip())
+    stopwords = stopwordslist('stop_word.txt')  # 这里加载停用词的路径
+    outstr = ''
+    for word in sentence_seged:
+        if word not in stopwords:
+            if word != '\t':
+                outstr += word
+                outstr += " "
+    return outstr
+
+
+inputs = open('万晓利歌词汇总.txt', 'r', encoding='gbk')
+outputs = open('万晓利歌词汇总_outputs.txt', 'w', encoding='gbk')
+for line in inputs:
+    line_seg = seg_sentence(line)  # 这里的返回值是字符串
+    outputs.write(line_seg + '\n')
+outputs.close()
+inputs.close()
