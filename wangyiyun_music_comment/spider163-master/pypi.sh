ea0d8df9dc9596f39ca729a5ef0e9d8a5aa0e9e6
cassieric <2352180977@qq.com>
2019-03-28 20:09:10 +0800

    rm folder spider163

--- wangyiyun_music_comment/spider163-master/pypi.sh
+++
@@ -1,4 +0,0 @@
-python setup.py clean
-python setup.py sdist
-twine upload dist/*
-python setup.py clean
--- wangyiyun_music_comment/spider163-master/spider163/utils/const.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-RETURN_JSON = "return json data"
-RETURE_HTML = "return html data"
