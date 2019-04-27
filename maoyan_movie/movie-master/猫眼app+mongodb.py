0004814a4bb8d27a06015b01bf1e1bca4950a320
Python_web crawler <31296722+cassieeric@users.noreply.github.com>
2019-04-27 22:08:26 +0800

    Update 猫眼app+mongodb.py

--- maoyan_movie/movie-master/猫眼app+mongodb.py
+++ maoyan_movie/movie-master/猫眼app+mongodb.py
@@ -10,6 +10,7 @@ from urllib import parse
 collection=MongoClient('localhost',27017)
 collection=collection.movie
 collection=collection['毒液']
+
 def get_comment(url):
     useragnet=[
         'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
