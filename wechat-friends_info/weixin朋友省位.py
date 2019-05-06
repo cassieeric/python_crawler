768055388c242b04f65dd98d6c308d3200056d55
Python_web crawler <31296722+cassieeric@users.noreply.github.com>
2019-05-06 14:57:57 +0800

    Update weixin朋友省位.py

--- wechat-friends_info/weixin朋友省位.py
+++ wechat-friends_info/weixin朋友省位.py
@@ -2,12 +2,16 @@
 import itchat
 import pandas as pd
 
-itchat.login()
-friends = itchat.get_friends(update=True)
-df_friends = pd.DataFrame(friends)
-Province = df_friends.Province
-Province_count = Province.value_counts()
-# 有一些好友地理信息为空，过滤掉这一部分人。
-Province_count = Province_count[Province_count.index != '']
-print(Province_count)
+# 获取省位信息
+def get_province():
+    itchat.login()
+    friends = itchat.get_friends(update=True)
+    df_friends = pd.DataFrame(friends)
+    Province = df_friends.Province
+    Province_count = Province.value_counts()
+    # 有一些好友地理信息为空，过滤掉这一部分人。
+    Province_count = Province_count[Province_count.index != '']
+    print(Province_count)
 
+# 运行程序，扫码登录
+get_province()
